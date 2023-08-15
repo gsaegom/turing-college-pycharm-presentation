e407d2706c4a7a184b81ba7242254476850f0ab5
gsaegom <guillermosaezgomez@gmail.com>
2023-08-15 19:39:30 +0200

    First commit.

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 
+
 def get_number_passengers(df: pd.DataFrame, sex: str, p_class: int):
     return df[(df['Sex'] == sex) & (df['Pclass'] == p_class)].size
 
@@ -11,5 +12,7 @@ def is_passenger_id_even(passenger_id: int):
 
 def get_passenger_surname(name: str):
     return name.split(',')[0]
+
+
 def get_passenger_first_name(name: str):
     return name.split('.')[1]
